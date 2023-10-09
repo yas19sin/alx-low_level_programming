b1349abbbb88c7081fcd01a56255bfd5cbc42362
yas19sin <yas19sin@gmail.com>
2023-10-09 15:18:32 -0700

    fix typo betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints all single-digit numbers of base 10 
+ * main - Prints all single-digit numbers of base 10
  * starting from 0, followed by a new line.
  *
  * Return: Always 0 (Success)
