3c0c7be195c51951e12588315096fe1684d4f158
yas19sin <yas19sin@gmail.com>
2023-10-10 19:40:06 -0700

    fixed and advanced

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
- * print_last_digit - Prints the last digit of a number.
- * @n: The number to extract the last digit from.
+ * print_last_digit - prints the last digit of a number
+ * @n: the integer to extract the last digit from
  *
- * Return: The value of the last digit.
-*/
+ * Return: the value of the last digit
+ */
 int print_last_digit(int n)
 {
-	int last_digit = _abs(n) % 10;
-	_putchar(last_digit + '0');
-	return last_digit;
+	int last_digit;
+
+	if (n < 0)
+		n = -n;
+
+	last_digit = n % 10;
+
+	_putchar('0' + last_digit);
+
+	return (last_digit);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - Prints the 9 times table starting from 0.
+ * times_table - prints the 9 times table
 */
 void times_table(void)
 {
@@ -8,28 +8,22 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0');
+
+		for (column = 1; column <= 9; column++)
 		{
+			_putchar(',');
+
 			product = row * column;
-			if (column == 0)
-            {
-				_putchar(product + '0');
-			}
-			else
+
+			if (product < 10)
 			{
 				_putchar(' ');
-				if (product < 10)
-					_putchar(' ');
-				else
-					_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
 			}
-			if (column < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+
+			_putchar('0' + product);
 		}
+
 		_putchar('\n');
 	}
 }
