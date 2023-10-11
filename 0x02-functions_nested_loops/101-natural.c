fdf07697fd689a35b468c12b130d41254395ab7c
yas19sin <yas19sin@gmail.com>
2023-10-11 15:01:58 -0700

    final

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,10 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
- *
- * Return: Always 0
+ * Description: compute and print the sum of all the multiples of 3 or
+ * 5 below 1024, followed by a new line.
+ * Return: 0 (Success)
 */
 int main(void)
 {
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,19 +10,38 @@
 */
 int main(void)
 {
-	int count;
-	unsigned long int a = 1, b = 2, next;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%lu, %lu", a, b);
+	printf("%lu", bef);
 
-	for (count = 2; count < 98; count++)
+	for (i = 1; i < 91; i++)
 	{
-		next = a + b;
-		printf(", %lu", next);
-		a = b;
-		b = next;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
 	printf("\n");
 	return (0);
 }
