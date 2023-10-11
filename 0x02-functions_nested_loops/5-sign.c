36e8a7bbd997acd6ff318acd0ecbfc4005a9335a
yas19sin <yas19sin@gmail.com>
2023-10-10 19:22:40 -0700

    finished mandatory tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 and print + if n is positive, 0 and print 0 if n is zero,
+ *         -1 and print - if n is negative.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table starting from 0.
+*/
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			if (column == 0)
+            {
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				else
+					_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
