c5ee0834265bdf0e718689413fecb10f52db6989
yas19sin <yas19sin@gmail.com>
2023-10-25 19:38:17 -0700

    fixed/added forgotten stuff

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,3 +26,15 @@ int check_palindrome(char *s, int start, int end)
 		return (0);
 	return (check_palindrome(s, start + 1, end - 1));
 }
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to measure.
+ * Return: The length of the string.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
