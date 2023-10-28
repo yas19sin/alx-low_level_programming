fe298e28fb2b31af26ba46508fe42bb3ccf6ecb8
yas19sin <yas19sin@gmail.com>
2023-10-28 15:15:12 -0700

    stop

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,10 +8,9 @@
 */
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
-	return (0);
+        for (int i = 0; i < argc; i++)
+        {
+                printf("%s\n", argv[i]);
+        }
+        return (0);
 }
