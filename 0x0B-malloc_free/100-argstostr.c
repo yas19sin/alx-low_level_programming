379b42e290d3f1bfbbcbb37beaabf0e8513bd0b2
yas19sin <yas19sin@gmail.com>
2023-10-31 18:09:18 -0700

    fix 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * argstostr - Concatenates all the arguments of the program
- * @ac: The number of arguments
- * @av: An array of arguments
- * Return: A pointer to the concatenated string, or NULL on failure
+ * argstostr - Concatenates all the arguments of your program.
+ * @ac: The argument count
+ * @av: The argument vector
+ *
+ * Return: A pointer to the concatenated string, or NULL if it fails
 */
 char *argstostr(int ac, char **av)
 {
 	char *concatenated;
-	int i, j, k, len = 0, totalLen = 0;
+	int i, k, len = 0, totalLen = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
-		{
+		for (k = 0; av[i][k] != '\0'; k++)
 			len++;
-			totalLen++;
-		}
+		totalLen += len + 1;
+		len = 0;
 	}
 
-	concatenated = (char *)malloc(totalLen + ac + 1);
-
+	concatenated = malloc((totalLen + 1) * sizeof(char));
 	if (concatenated == NULL)
 		return (NULL);
 
-	for (i = 0, k = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
-		len = 0;
-		while (av[i][len])
-		{
-			concatenated[k] = av[i][len];
-			len++;
-			k++;
-		}
-		concatenated[k] = '\n';
-		k++;
+		for (k = 0; av[i][k] != '\0'; k++)
+			concatenated[len++] = av[i][k];
+		concatenated[len++] = '\n';
 	}
-	concatenated[k] = '\0';
+
+	concatenated[len] = '\0';
 
 	return (concatenated);
 }
