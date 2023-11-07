002e68eaaa6d89056ac31e8bc8ad25d020b49eaf
yas19sin <yas19sin@gmail.com>
2023-11-06 17:28:30 -0800

    0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf(__FILE__ "\n");
+	return (0);
+}
