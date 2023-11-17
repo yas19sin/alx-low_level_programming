6241869eaa6748b9e990eccd33dca7648bb80019
yas19sin <yas19sin@gmail.com>
2023-11-16 22:10:06 -0800

    bruh

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,17 +2,15 @@ section .data
 	hello db 'Hello, Holberton', 0
 
 section .text
-	global _start
+	global main
 
-_start:
-	; write hello string to stdout
+main:
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, hello
 	mov edx, 17
 	int 0x80
 
-	; exit
 	mov eax, 1
 	xor ebx, ebx
 	int 0x80
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
