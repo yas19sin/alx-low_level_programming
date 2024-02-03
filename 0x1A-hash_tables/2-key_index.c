8606288901aa880982a51b9f0f663725ee401a6a
yas19sin <yas19sin@gmail.com>
2024-02-03 02:01:21 +0100

    fixed

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,5 +9,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int id;
+
+	id = hash_djb2(key);
+
+	return (id % size);
 }
